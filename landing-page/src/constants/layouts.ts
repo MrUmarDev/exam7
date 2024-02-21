0e31bfb77a5a92b6597793b0e3a8db2185c1fa2f
UMAR <mrumardev@gmail.com>
2024-02-21 11:13:15 -0800

    Delete landing-page directory

--- landing-page/src/constants/layouts.ts
+++
@@ -1,5 +0,0 @@
-const ADMIN = 'Admin';
-const USER = 'User';
-const AUTH = 'Auth';
-
-export { ADMIN, USER, AUTH };
--- landing-page/src/interfaces/IFilters.ts
+++
@@ -1,3 +0,0 @@
-export default interface IFilters {
-  formatCategory: (category: string) => string;
-}
